9abce29dce0421d314bb855c525c3a05b12ef5cd
Dugidem Egwuatu <Dugidemeg@gmail.com>
2024-07-02 14:36:42 +0100

    menu html,css,js & responsiveness

---
+++ agency.js
@@ -0,0 +1,30 @@
+// function openNav(){
+//     document.getElementById("navbar-mobile").style.width = "50%";
+//     document.getElementById("navlinks-mobile").style.display = "flex";
+    
+// }
+
+// function closeNav(){
+//     document.getElementById("navbar-mobile").style.width = "";
+      
+// }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function openNav() {
+  document.getElementById("navlinks-mobile").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.hamburger')) {
+    let navlinks = document.getElementsByClassName("nav-links");
+    let i;
+    for (i = 0; i < navlinks.length; i++) {
+      let openNavlinks = navlinks[i];
+      if (openNavlinks.classList.contains('show')) {
+        openNavlinks.classList.remove('show');
+      }
+    }
+  }
+}
